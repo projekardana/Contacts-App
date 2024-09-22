d0455ccc3b418aa6881ff3a6ca8fa2ae70286c58
mdendiardana <mdendiardana@gmail.com>
2024-09-22 23:16:10 +0700

    first-commit

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const getData = () => {
+    return [
+        {
+            id: 1,
+            name: 'Muhammad Dendi Ardana',
+            tag: 'mdendiardana',
+            imgURL: '/public/images/dimasmds.jpeg',
+        },
+        {
+            id: 2,
+            name: 'Arif Faizin',
+            tag: 'arifaizin',
+            imgURL: '/public/images/arifaizin.jpeg',
+        },
+        {
+            id: 3,
+            name: 'Rahmat Fajri',
+            tag: 'rfajri27',
+            imgURL: '/public/images/rfajri27.jpeg'
+        },
+    ];
+}
+
+export {getData};
